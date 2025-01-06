f617be9478410dfb43c71da797bb0c1a2b854760
grec88 <Lihtinmann88@yandex.ru>
2025-01-07 00:06:43 +0300

    feat: countCats function is implemented

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -13,22 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function getCommonCharacterCount(s1, s2) {
   let result = 0;
-  const count1 = s1.split('').reduce((acc, val) =>{
+  const count1 = s1.split("").reduce((acc, val) => {
     acc[val] = acc[val] ? acc[val] + 1 : 1;
     return acc;
   }, {});
-  const count2 = s2.split('').reduce((acc, val) =>{
+  const count2 = s2.split("").reduce((acc, val) => {
     acc[val] = acc[val] ? acc[val] + 1 : 1;
     return acc;
   }, {});
-  for(const val1 in count1){
-      if(count2[val1]){
-        result += Math.min(count1[val1], count2[val1]);
-      }
+  for (const val1 in count1) {
+    if (count2[val1]) {
+      result += Math.min(count1[val1], count2[val1]);
+    }
   }
   return result;
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
+
